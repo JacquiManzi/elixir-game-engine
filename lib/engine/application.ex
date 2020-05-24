8bcaa9bce43e3b6b06e3433fffb871559290ad49
Jacqui <jacquimanzi@gmail.com>
2020-05-23 20:37:49 -0700

    Configuration

---
+++ lib/engine/application.ex
@@ -0,0 +1,28 @@
+defmodule Engine.Application do
+  @moduledoc false
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+    opts = [strategy: :one_for_one, name: Engine.Supervisor]
+    HTTPoison.start()
+
+    children =
+      [
+      ] ++ children(target())
+
+    Supervisor.start_link(children, opts)
+  end
+
+  def children(:host) do
+    []
+  end
+
+  def children(_target) do
+    []
+  end
+
+  def target() do
+    Application.get_env(:hello_nerves, :target)
+  end
+end
